a1a6e0b739c0c0c1230e83bee4f7c61a97248a82
Bhavukjindal <54846776+Bhavukjindal@users.noreply.github.com>
2022-09-15 12:07:26 +0530

    Assignment_1_Solutions

---
+++ Assignment_1/Question_2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Question_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        float num1 = sc.nextFloat();
+        System.out.println("Enter Second number: ");
+        float num2 = sc.nextFloat();
+
+        float product = num1*num2;
+
+        System.out.println("The product is: " + product);
+    }
+}
---
+++ Assignment_1/Question_4.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Question_4 {
+    public static void Swap_using_temp(int num1,int num2){
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("The First number is: " + num1);
+        System.out.println("The Second number is: " + num2);
+    }
+    public static void Swap_without_using_temp(int num1,int num2){
+        num1 = num1+num2;
+        num2 = num1-num2;
+        num1 = num1-num2;
+        System.out.println("The First number is: " + num1);
+        System.out.println("The Second number is: " + num2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter Second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter 1 to swap using temporary var and 0 to swap without using temporary var: ");
+        
+
+        int c = sc.nextInt();
+        if(c==1){
+            Swap_using_temp(num1, num2);
+        }else{
+            Swap_without_using_temp(num1, num2);
+        }
+        
+    }
+}
